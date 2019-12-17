3c10c2f0c4918ed66d1747c59778e63787c883b8
Leonardo Grasso <me@leonardograsso.com>
2019-12-17 17:19:33 +0100

    feat(extractor): object metadata extractor

---
+++ pkg/extractor/extractor.go
@@ -0,0 +1,55 @@
+/*
+Copyright 2019 vChain, Inc.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+	http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package extractor
+
+import (
+	"fmt"
+
+	"github.com/codenotary/objects/pkg/object"
+	"github.com/codenotary/objects/pkg/uri"
+)
+
+var extractors = map[string]Extractor{}
+
+// Extractor extract an object.Object referenced by the given uri.URI.
+type Extractor func(uri.URI, ...Option) (*object.Object, error)
+
+// Register the Extractor e for the given scheme
+func Register(scheme string, e Extractor) {
+	extractors[scheme] = e
+}
+
+// Schemes returns the list of registered schemes.
+func Schemes() []string {
+	schemes := make([]string, len(extractors))
+	i := 0
+	for scheme := range extractors {
+		schemes[i] = scheme
+		i++
+	}
+	return schemes
+}
+
+// Extract returns an object.Object for the given rawURI.
+func Extract(rawURI string, options ...Option) (*object.Object, error) {
+	u, err := uri.Parse(rawURI)
+	if err != nil {
+		return nil, err
+	}
+
+	if e, ok := extractors[u.Scheme]; ok {
+		return e(*u, options...)
+	}
+	return nil, fmt.Errorf("%s scheme not yet supported", u.Scheme)
+}
---
+++ pkg/extractor/options_test.go
@@ -0,0 +1,56 @@
+/*
+Copyright 2019 vChain, Inc.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+	http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package extractor
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type dummyOpts struct {
+	flag bool
+}
+
+func withTestOption() Option {
+	return func(o interface{}) error {
+		if oo, ok := o.(*dummyOpts); ok {
+			oo.flag = true
+		}
+		return nil
+	}
+}
+
+func withTestOptionWithError() Option {
+	return func(o interface{}) error {
+		return errors.New("some error")
+	}
+}
+
+func TestApply(t *testing.T) {
+
+	opts := &dummyOpts{}
+
+	err := Options([]Option{
+		withTestOption(),
+	}).Apply(opts)
+	assert.NoError(t, err)
+	assert.True(t, opts.flag)
+
+	err = Options([]Option{
+		withTestOptionWithError(),
+	}).Apply(opts)
+	assert.Error(t, err)
+}
